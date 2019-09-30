6076242f6af67dff79677267044728bb7e50f28c
Maxwell <msl@lclark.edu>
2019-09-30 12:49:12 -0700

    Added instructions

--- auth.py
+++ auth.py
@@ -1,4 +1,6 @@
 # Parameters used for authorization with unsplash api. 
+# If using git, you can tell git to ignore future changes to this file by running:
+# git update-index --assume-unchanged auth.py
 
 # API keys for Unsplash
 ACCESS_KEY = 'YOUR ACCESS KEY'
